b4ed365cb679c62220f2e0278f608e412ccf9bba
MattiasL2001 <mattias-lindblad2001@hotmail.com>
2024-06-06 12:26:24 +0200

    Updated Home page, added header and footer, updated styling.

--- WebShop_Frontend/src/App.tsx
+++ WebShop_Frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
-import './styles/App.css';
+import Register from './views/Register';
+import Login from './views/Login';
+import Support from './views/Support';
+import NoPage from './views/NoPage';
+import './styles/styles.css';
 
 function App() {
   return (
@@ -10,8 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/support" element={<Support />} />
         <Route path="/unauthorized" element={<UnauthorizedPage />} />
-        {/* <Route path='*' element={<NoPage />} /> */}
+        <Route path='*' element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   </div>
