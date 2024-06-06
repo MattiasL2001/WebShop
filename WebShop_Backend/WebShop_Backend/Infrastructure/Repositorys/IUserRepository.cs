20aa2a21fe22ffc0a46b906d49948f2e7a18c733
marcuszxc <marcuszxc10@gmail.com>
2024-06-06 11:34:38 +0200

    Added Sqllite db and repositorys

---
+++ WebShop_Backend/WebShop_Backend/Infrastructure/Repositorys/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using WebShop_Backend.Entity;
+
+namespace WebShop_Backend.Infrastructure.Repositorys
+{
+    public interface IUserRepository
+    {
+        public Task<User> CreateUser(User user);
+        public Task<User> GetUser(int id);
+        public Task<User> UserLogin(User user);
+        public Task<List<int>> GetBasket(int userId);
+        public Task<List<int>> AddToBasket(int userId, int productId);
+        public Task<bool> RemoveFromBasket(int userId, int productId);
+
+
+    }
+}
---
+++ WebShop_Backend/WebShop_Backend/Migrations/20240606084810_InitialCreate.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebShop_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PriceTotal = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ProductAmount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Basket = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
