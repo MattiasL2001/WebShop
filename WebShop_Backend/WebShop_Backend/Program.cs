24e3d3320850a82da9696068d6d7a386ad6fc541
marcuszxc <marcuszxc10@gmail.com>
2024-06-06 11:23:14 +0200

    Changed backend configuration and files

--- WebShop_Backend/WebShop_Backend/Program.cs
+++ WebShop_Backend/WebShop_Backend/Program.cs
@@ -1,11 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WebShop_Backend.Helpers;
+using WebShop_Backend.Infrastructure;
+using WebShop_Backend.Infrastructure.Repositorys;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
+builder.Services.AddDbContext<WebShopContext>(options =>
+    options.UseSqlite(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfiler));
 
 var app = builder.Build();
 
@@ -21,5 +37,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("API starting...");
 
 app.Run();
