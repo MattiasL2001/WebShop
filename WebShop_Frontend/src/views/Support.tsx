248bba0bf8a780309c20f9b7d13fe337416724fc
MattiasL2001 <mattias-lindblad2001@hotmail.com>
2024-06-06 12:44:49 +0200

    Added tsx pages, removed unused files, changed logo and title, added Sass script.

---
+++ WebShop_Frontend/src/views/Support.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import '../styles/styles.css';
+
+const Webstore: React.FC = () => {
+
+
+  return (
+    <>
+
+      <Header></Header>
+
+      <div id="supportText">
+        <h2>
+            There is currently no support available. What did you expect?
+        </h2>
+      </div>
+
+    </>
+  );
+};
+
+export default Webstore;
