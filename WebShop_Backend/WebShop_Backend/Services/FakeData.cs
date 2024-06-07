1bf99d42e01c14507a71e2568c8f41667b9bfc51
MattiasL2001 <mattias-lindblad2001@hotmail.com>
2024-06-07 13:14:04 +0200

    created fake data and adjusted the name

--- WebShop_Backend/WebShop_Backend/Services/FakeData.cs
+++ WebShop_Backend/WebShop_Backend/Services/FakeData.cs
@@ -34,16 +34,16 @@ namespace WebShop_Backend.Services
                 .RuleFor(p => p.Id, f => f.IndexFaker + 1)
                 .RuleFor(p => p.Name, f => f.Commerce.ProductName())
                 .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
-                .RuleFor(p => p.Price, f => f.Random.Decimal(1, 1000))
+                .RuleFor(p => p.Price, f => decimal.Parse(f.Random.Decimal(1, 1000).ToString("0.00")))
                 .RuleFor(p => p.ProductAmount, f => f.Random.Number(1, 100))
                 .RuleFor(p => p.ProductType, f => f.PickRandom<Product.Type>())
                 .RuleFor(p => p.ProductColor, f => f.PickRandom<Product.Color>())
                 .RuleFor(p => p.ProductGender, f => f.PickRandom<Product.Gender>());
 
-            var fakeProducts = _productFaker.Generate(profilesToGenerate * 2);
+            var fakeProducts = _productFaker.Generate(profilesToGenerate);
 
             var options = new DbContextOptionsBuilder<WebShopContext>()
-                .UseSqlite("Data Source=Instapot.db")
+                .UseSqlite("Data Source=WebShop.db")
                 .Options;
 
             using (var dbContext = new WebShopContext(options))
