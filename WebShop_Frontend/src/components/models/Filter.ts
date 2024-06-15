e90fad2621dace846307c677df50f706c27c8e49
marcuszxc <marcuszxc10@gmail.com>
2024-06-15 13:28:00 +0200

    I have reactifyde the filter selectors

---
+++ WebShop_Frontend/src/components/models/Filter.ts
@@ -0,0 +1,6 @@
+export const Filter = {
+    Gender:["Filter By Gender","Men","Women"],
+    Colors:[ "Filter By Color", "White", "Gray", "Black", "Red", "Green", "Yellow", "Orange", "Blue", "Pink", "Purple", "Brown" ],
+    ProductProperty:["Filter By Product", "Clothing", "Accessories"],
+    Sort:["Sort By", "Price, lowest first", "Price, highest first"]
+  }
--- WebShop_Frontend/src/views/Home.tsx
+++ WebShop_Frontend/src/views/Home.tsx
@@ -3,8 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import Page from '../components/Page';
 import { GetProducts } from '../services/webShopServices';
 import { Link } from 'react-router-dom';
+import { Filter } from '../components/models/Filter';
 import '../styles/styles.css';
 import '../styles/home.scss';
+import SelectFilter from '../components/partial components/SelectFilter';
+
 
 const Webstore: React.FC = () => {
   const { data: products, isLoading, error } = useQuery({
@@ -15,41 +18,17 @@ const Webstore: React.FC = () => {
   return (
     <Page>
       <div id="searchArticles">
-        <select name="gender" className="filter" id="filterGender">
-          <option value="gender">Filter By Gender</option>
-          <option value="men">Men</option>
-          <option value="women">Women</option>
-        </select>
+        
+      <SelectFilter filterItems={Filter.Gender}></SelectFilter>
 
-        <select name="product" className="filter">
-          <option value="">Filter By Product</option>
-          <option value="clothing">Clothing</option>
-          <option value="accessories">Accessories</option>
-        </select>
+      <SelectFilter filterItems={Filter.ProductProperty}></SelectFilter>
 
-        <select name="color" className="filter">
-          <option value="">Filter By Color</option>
-          <option value="white">White</option>
-          <option value="gray">Gray</option>
-          <option value="black">Black</option>
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="yellow">Yellow</option>
-          <option value="orange">Orange</option>
-          <option value="blue">Blue</option>
-          <option value="pink">Pink</option>
-          <option value="purple">Purple</option>
-          <option value="brown">Brown</option>
-        </select>
+      <SelectFilter filterItems={Filter.Colors}></SelectFilter>
 
-        <select name="sort" className="filter">
-          <option value="">SORT BY</option>
-          <option value="price-low">Price, lowest first</option>
-          <option value="price-high">Price, highest first</option>
-          <option value="name">Name</option>
-        </select>
+      <SelectFilter filterItems={Filter.Sort}></SelectFilter>
+
       </div>
-
+      
       <section id="articles">
         {products?.map((product: any) => (
           <Link to={`/product/${product.id}`} key={product.id} className="product">
