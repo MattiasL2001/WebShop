eaeca8cfc8bc08e3209a846d33760b4c13ad0aaf
marcuszxc <marcuszxc10@gmail.com>
2024-09-29 15:03:01 +0200

    WebShop_Backend: Added climes for name, email and birthdate

--- WebShop_Backend/WebShop_Backend/Authentication/Basic/BasicAuthenticationHandler.cs
+++ WebShop_Backend/WebShop_Backend/Authentication/Basic/BasicAuthenticationHandler.cs
@@ -12,46 +12,52 @@ namespace WebShop_Backend.Authentication.Basic
     {
 
         private readonly IAuthenticationRepository _authenticationRepository;
+        private readonly IUserRepository _userRepository;
 
-        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IAuthenticationRepository authenticationRepository) : base(options, logger, encoder, clock)
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IAuthenticationRepository authenticationRepository, IUserRepository userRepository) : base(options, logger, encoder, clock)
         {
             _authenticationRepository = authenticationRepository;
+            _userRepository = userRepository;
         }
 
-        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
             if (Request.Headers.ContainsKey("Authorization") == false)
             {
-                return Task.FromResult(AuthenticateResult.Fail("Missing Authorization key"));
+                return await Task.FromResult(AuthenticateResult.Fail("Missing Authorization key"));
             }
 
             var authorizationHeader = Request.Headers["Authorization"].ToString();
 
             if (authorizationHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase) == false)
             {
-                return Task.FromResult(AuthenticateResult.Fail("Authorization header does not start with 'Basic '"));
+                return await Task.FromResult(AuthenticateResult.Fail("Authorization header does not start with 'Basic '"));
             }
 
-            var authUser = _authenticationRepository.AuthenticateUser(authorizationHeader).Result;
+            var authUser = await _authenticationRepository.AuthenticateUser(authorizationHeader);
 
             if (authUser == null)
             {
-                return Task.FromResult(AuthenticateResult.Fail("Invalid Authorization header format"));
+                return await Task.FromResult(AuthenticateResult.Fail("Invalid Authorization header format"));
             }
+
+            var claim = await _userRepository.GetClaims(authUser.Email);
 
             var client = new AuthenticationClient
             {
                 AuthenticationType = BasicAuthenticationDefaults.AuthenticationScheme,
                 IsAuthenticated = true,
-                Name = authUser.Email,
+                Name = claim.name,
             };
 
             var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(client, new[]
             {
-                new Claim(ClaimTypes.Name, authUser.Email)
+                new System.Security.Claims.Claim(ClaimTypes.Name, claim.name),
+                new System.Security.Claims.Claim(ClaimTypes.Email, authUser.Email),
+                new System.Security.Claims.Claim(ClaimTypes.DateOfBirth, claim.birthDate.ToString()),
             }));
 
-            return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name)));
+            return await Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name)));
 
         }
     }
